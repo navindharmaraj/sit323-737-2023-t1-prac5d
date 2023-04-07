33ab2eadc03ae04a84ca5a27b8bb22223f1d6467
Navin Dharmarajan <navindharmaraj@users.noreply.github.com>
2023-04-07 23:45:57 +1000

    Docker Compose Update

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const axios = require('axios');
-
+const http = require('https');
 const app = express();
 
 // Endpoint to send GET request to other container
 app.get('/api/data', async (req, res) => {
   try {
-    const response = await axios.get('http://sit323-737-2023-t1-prac5d-my-other-container-1:5006/api/data');
+    const response = await axios.get('http://my-other-container:5006/data');
     res.send(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).send('Error getting data from other container');
   }
 });
+app.get('/data', async (req, res) => {
+    try {
+      const response = 'Hello there';
+      res.send(response);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error getting data from other container');
+    }
+  });
 app.get('/api/health', async (req, res) => {
     try {
         const status = {
